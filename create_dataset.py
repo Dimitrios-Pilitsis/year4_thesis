bece5e7be6f9b154821e0da0a5224609a8016658
Dimitrios Pilitsis <dimipili@gmail.com>
2022-03-19 22:25:30 +0000

    Remove duplicates after placeholders function call

--- create_dataset.py
+++ create_dataset.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
+import numpy as np
+
 import os
 
 from datasets import load_dataset
+
+import demoji
+import emoji
 
 # Important variables ------------------------------------------------------
 #TODO: make variables into arguments passed from calling program
@@ -13,18 +18,36 @@ dataset_complete_filepath = "./dataset/dataset_complete.csv"
 
 # Functions to clean datasets
 
+
+def camel_case_split(word):
+    start_idx = [i for i, e in enumerate(word) if e.isupper()] + [len(word)]
+    start_idx = [0] + start_idx
+    list_words = [word[x: y] for x, y in zip(start_idx, start_idx[1:])][1:] 
+    return ' '.join(list_words)
+
+#Deals with cases where there are consecutive emojis or no space between text
+#and emoji
+def emoji_present(text):
+    if emoji.is_emoji(text) or (len(text) > 1 and emoji.is_emoji(text[0])) or (len(text) > 1 and emoji.is_emoji(text[-1])):
+        return demoji.replace_with_desc(text, sep="")
+    else:
+        return text
+
 def placeholders(texts):
     for count, text in enumerate(texts):
         new_text = []
         for t in text.split(" "):
-            t = '@user' if t.startswith('@') and len(t) > 1 else t
+            t = '' if t.startswith('@') and len(t) > 1 else t
             t = 'http' if t.startswith('http') else t
             t = "" if "RT" in t else t
+            t = camel_case_split(t) if t.startswith('#') and len(t) > 1 else t
+            t = emoji_present(t)
             new_text.append(t)
 
-        texts[count] = " ".join(new_text)
+        texts[count] = " ".join(new_text).strip()
 
     return texts
+
 
 def clean_individual_dataset(filepath):
     df = pd.read_csv(filepath, sep="\t", header=0)
@@ -50,19 +73,21 @@ def clean_individual_dataset(filepath):
 
     # Add placeholders and remove unnecessary text
     df['tweet_text'] = placeholders(df['tweet_text'])
+
+    #Remove empty tweets
+    df['tweet_text'].replace('', np.nan, inplace=True) 
+    df.dropna(subset=['tweet_text'], inplace=True)
+
     
     return df
 
 
-def check_individual_dataset(filepath):
+def check_individual_dataset():
+    file_path = "/home/dimipili/Documents/Documents/my_folder/University/Year_4/Thesis/year4_thesis/dataset/CrisisNLP_labeled_data_crowdflower/2013_Pakistan_eq/2013_Pakistan_eq_CF_labeled_data.tsv"
     df = clean_individual_dataset(filepath)
     print(df)
-    print((df['tweet_text'][5]))
-    #print(df)
+    print((df['tweet_text'][1361]))
 
-example = "/home/dimipili/Documents/Documents/my_folder/University/Year_4/Thesis/year4_thesis/dataset/CrisisNLP_labeled_data_crowdflower/2013_Pakistan_eq/2013_Pakistan_eq_CF_labeled_data.tsv"
-check_individual_dataset(example)
-exit(0)
 
 
 
@@ -75,9 +100,14 @@ def obtain_filepaths(directory_of_datasets):
                 filepaths.append(os.path.join(subdir, file))
     return filepaths
 
+
 def check_for_duplicate_tweets(df):
     df.astype({'text': 'string'})
-    print(df[df.duplicated(keep=False, subset=['text'])])
+    df.drop_duplicates(subset=['text'], inplace=True)
+    #Checks for duplicates
+    #duplicates = df[df.duplicated(keep="first", subset=['text'])]
+    return df
+
 
 def data_fusion(directory_of_dataset, dataset_complete_filepath):
     dataframes = []
@@ -92,7 +122,7 @@ def data_fusion(directory_of_dataset, dataset_complete_filepath):
     df_total.rename(columns={"tweet_text": "text"}, inplace=True)
 
     #Check for duplicate tweets
-    check_for_duplicate_tweets(df_total)
+    df_total = check_for_duplicate_tweets(df_total)
     
     df_total.to_csv(dataset_complete_filepath, index=False)
 
@@ -128,6 +158,8 @@ def save_dataset_apache_arrow(data):
 run_create_csv(directory_of_original_datasets, dataset_complete_filepath)
 
 data = split_dataset(dataset_complete_filepath)
+
+# Check random points
 #print(data['train'].select(range(3))['text'])
 
 save_dataset_apache_arrow(data)
