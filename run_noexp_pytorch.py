ead4a328d25cfed7c2a347bda4efd7edb7b38208
Dimitrios Pilitsis <dimipili@gmail.com>
2022-03-20 00:03:27 +0000

    Move core functionality to main function

--- run_noexp_pytorch.py
+++ run_noexp_pytorch.py
@@ -21,6 +21,8 @@ from tqdm.auto import tqdm
 
 from accelerate import Accelerator
 
+
+
 # Summary writer helper functions --------------------------------------------
 def get_summary_writer_log_dir(log_dir) -> str:
     """Get a unique directory that hasn't been logged to before for use with a TB
@@ -40,99 +42,16 @@ def get_summary_writer_log_dir(log_dir) -> str:
         i += 1
     return str(tb_log_dir)
 
-# Logger ---------------------------------------------------------------
-
-logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    datefmt="%m/%d/%Y %H:%M:%S",
-    level=logging.INFO,
-)
-
-logs_directory = get_summary_writer_log_dir(Path('logs'))
-
-summary_writer = SummaryWriter(str(logs_directory), flush_secs=5)
-
-accelerator = Accelerator()
-
-logger.info(accelerator.state)
 
 
-full_logger = False
+# Tokeinzer functions -------------------------------------------------
 
-if full_logger:
-    # Setup logging, we only want one process per machine to log things on the
-    # screen.
-    # accelerator.is_local_main_process is only True for one process per machine.
-
-
-    logger.setLevel(logging.INFO if accelerator.is_local_main_process 
-        else logging.ERROR)
-
-    if accelerator.is_local_main_process:
-        datasets.utils.logging.set_verbosity_warning()
-        transformers.utils.logging.set_verbosity_info() #outputs model config
-    else:
-        datasets.utils.logging.set_verbosity_error()
-        transformers.utils.logging.set_verbosity_error()
-
-
-
-# Important variables and flags --------------------------------------------
-# TODO: turn variables into arguments passed from calling program
-flag_smaller_datasets = True
-
-num_epochs = 3
-batch_size=8
-
-output_directory_save_model = "./saved_model/"
-
-checkpoint = "bert-base-cased"
-
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint,
-    num_labels=8)
-
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-
-# Loading dataset ---------------------------------------------
-
-raw_datasets = load_from_disk("./dataset/crisis_dataset")
-
-
-# Log a few random samples from the training set:
-for index in random.sample(range(len(raw_datasets)), 3):
-    logger.info(f"Data point {index} of the training set sample: {raw_datasets['train'][index]['text']}.")
-
-
-# Tokenizers ----------------------------------------------------
-#Is dataset specific
-def tokenize_function(examples):
-    return tokenizer(examples["text"], truncation=True)
-
-
-tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-
-
-#Remove columns that aren't strings here
-tokenized_datasets = tokenized_datasets.remove_columns(["text"])
-
-
-#Collator function for padding
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 
 def decode_text(tokenizer, text):
     encoded_input = tokenizer(text)
     decoded_text = tokenizer.decode(encoded_input["input_ids"])
     return decoded_text
-
-
-# Log a few random samples from the tokenized dataset:
-for index in random.sample(range(len(tokenized_datasets)), 3):
-    logger.info(f"Data point {index} of the tokenized training set sample: {decode_text(tokenizer, raw_datasets['train'][index]['text'])}.")
-
 
 
 # Smaller datasets to speed up training ----------------------------
@@ -148,46 +67,12 @@ def create_smaller_dataset(tokenized_datasets):
         "validation": small_validation_dataset, "test": small_test_dataset})
     return small_datasets
 
-if flag_smaller_datasets:
-    tokenized_datasets = create_smaller_dataset(tokenized_datasets)
-
-
-tokenized_datasets.set_format("torch")
-
-
-
-# Dataloader --------------------------------------------------
-
-train_dataloader = DataLoader(tokenized_datasets['train'], shuffle=True, 
-    batch_size=batch_size, collate_fn=data_collator)
-eval_dataloader = DataLoader(tokenized_datasets['validation'],
-    batch_size=batch_size, collate_fn=data_collator)
-test_dataloader = DataLoader(tokenized_datasets['test'], batch_size=batch_size, 
-    collate_fn=data_collator)
-
-
-# Optimizer and learning rate scheduler -----------------------------
-
-optimizer = AdamW(model.parameters(), lr=5e-5)
-
-train_dataloader, eval_dataloader, model, optimizer = accelerator.prepare(
-    train_dataloader, eval_dataloader, model, optimizer
-)
-
-num_training_steps = num_epochs * len(train_dataloader)
-
-lr_scheduler = get_scheduler(
-    name="linear", 
-    optimizer=optimizer, 
-    num_warmup_steps=0,
-    num_training_steps=num_training_steps
-)
 
 
 
 # Metrics -----------------------------------------------------
 
-def compute_metrics(model, dataloader):
+def compute_metrics(accelerator, model, dataloader):
     metric1 = load_metric("accuracy")
     metric2 = load_metric("f1")
     metric3 = load_metric("f1")
@@ -217,57 +102,8 @@ def compute_metrics(model, dataloader):
         "f1_macro": f1_macro}
     return results
 
-        
 
 
-
-#Training loop -------------------------------------------------------------
-
-logger.info("***** Running training *****")
-logger.info(f"  Num examples = {tokenized_datasets['train'].num_rows}")
-logger.info(f"  Num Epochs = {num_epochs}")
-logger.info(f"  Num training steps = {num_training_steps}")
-logger.info(f"  Instantaneous batch size per device = {batch_size}")
-progress_bar = tqdm(range(num_training_steps), disable=not accelerator.is_local_main_process)
-
-for epoch in range(num_epochs):
-    model.train()
-    for batch in train_dataloader:
-        outputs = model(**batch)
-        loss = outputs.loss
-        accelerator.backward(loss)
-
-
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
-        progress_bar.update(1)
-
-
-    train_metrics = compute_metrics(model, train_dataloader)
-    logger.info(f"Epoch {epoch + 1} results: {train_metrics}")
-    summary_writer.add_scalar('accuracy/train', train_metrics['accuracy'], epoch)
-    summary_writer.add_scalar('f1_weighted/train', train_metrics['f1_weighted'], epoch)
-    summary_writer.add_scalar('f1_macro/train', train_metrics['f1_macro'], epoch)
-
-
-
-
-# Evaluation and testing -------------------------
-logger.info("***** Running evaluation set *****")
-eval_metrics = compute_metrics(model, eval_dataloader)
-logger.info(f"Evalation results: {eval_metrics}")
-
-
-logger.info("***** Running test set *****")
-test_metrics = compute_metrics(model, test_dataloader)
-logger.info(f"Test results: {test_metrics}")
-summary_writer.add_scalar('accuracy/eval', test_metrics['accuracy'], epoch)
-summary_writer.add_scalar('f1_weighted/eval', test_metrics['f1_weighted'], epoch)
-summary_writer.add_scalar('f1_macro/eval', test_metrics['f1_macro'], epoch)
-
-
-summary_writer.close()
 
 # Saving model --------------------------------------------------
 
@@ -275,6 +111,179 @@ def save_model(output_directory_save_model):
     model.save_pretrained(output_directory_save_model)
     tokenizer.save_pretrained(output_directory_save_model)
 
-#save_model(output_directory_save_model)
 
 
+
+
+
+
+def main():
+    # Logger and summary writer --------------------------------------------
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    logs_directory = get_summary_writer_log_dir(Path('logs'))
+
+    summary_writer = SummaryWriter(str(logs_directory), flush_secs=5)
+
+    accelerator = Accelerator()
+
+    logger.info(accelerator.state)
+
+    full_logger = False
+
+    if full_logger:
+        # Setup logging, we only want one process per machine to log things on the
+        # screen.
+        # accelerator.is_local_main_process is only True for one process per machine.
+
+        logger.setLevel(logging.INFO if accelerator.is_local_main_process 
+            else logging.ERROR)
+
+        if accelerator.is_local_main_process:
+            datasets.utils.logging.set_verbosity_warning()
+            transformers.utils.logging.set_verbosity_info() #outputs model config
+        else:
+            datasets.utils.logging.set_verbosity_error()
+            transformers.utils.logging.set_verbosity_error()
+
+
+
+    # Important variables and flags --------------------------------------------
+    # TODO: turn variables into arguments passed from calling program
+    flag_smaller_datasets = True
+
+    num_epochs = 3
+    batch_size=8
+
+    output_directory_save_model = "./saved_model/"
+
+    checkpoint = "bert-base-cased"
+
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint,
+        num_labels=8)
+
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+    # Loading dataset ---------------------------------------------
+
+    raw_datasets = load_from_disk("./dataset/crisis_dataset")
+
+    # Log a few random samples from the training set:
+    for index in random.sample(range(len(raw_datasets)), 3):
+        logger.info(f"Data point {index} of the training set sample: {raw_datasets['train'][index]['text']}.")
+
+
+    # Tokenizers ----------------------------------------------------
+    #Is dataset specific
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], truncation=True)
+
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+
+    #Remove columns that aren't strings here
+    tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+
+
+    #Collator function for padding
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+    # Log a few random samples from the tokenized dataset:
+    for index in random.sample(range(len(tokenized_datasets)), 3):
+        logger.info(f"Data point {index} of the tokenized training set sample: {decode_text(tokenizer, raw_datasets['train'][index]['text'])}.")
+
+    if flag_smaller_datasets:
+        tokenized_datasets = create_smaller_dataset(tokenized_datasets)
+
+
+    tokenized_datasets.set_format("torch")
+
+
+
+    # Dataloader --------------------------------------------------
+
+    train_dataloader = DataLoader(tokenized_datasets['train'], shuffle=True, 
+        batch_size=batch_size, collate_fn=data_collator)
+    eval_dataloader = DataLoader(tokenized_datasets['validation'],
+        batch_size=batch_size, collate_fn=data_collator)
+    test_dataloader = DataLoader(tokenized_datasets['test'], batch_size=batch_size, 
+        collate_fn=data_collator)
+
+
+    # Optimizer and learning rate scheduler -----------------------------
+
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+
+    train_dataloader, eval_dataloader, model, optimizer = accelerator.prepare(
+        train_dataloader, eval_dataloader, model, optimizer
+    )
+
+    num_training_steps = num_epochs * len(train_dataloader)
+
+    lr_scheduler = get_scheduler(
+        name="linear", 
+        optimizer=optimizer, 
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps
+    )
+
+
+    #Training loop -------------------------------------------------------------
+
+    logger.info("***** Running training *****")
+    logger.info(f"  Num examples = {tokenized_datasets['train'].num_rows}")
+    logger.info(f"  Num Epochs = {num_epochs}")
+    logger.info(f"  Num training steps = {num_training_steps}")
+    logger.info(f"  Instantaneous batch size per device = {batch_size}")
+    progress_bar = tqdm(range(num_training_steps), disable=not accelerator.is_local_main_process)
+
+    for epoch in range(num_epochs):
+        model.train()
+        for batch in train_dataloader:
+            outputs = model(**batch)
+            loss = outputs.loss
+            accelerator.backward(loss)
+
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+
+
+        train_metrics = compute_metrics(accelerator, model, train_dataloader)
+        logger.info(f"Epoch {epoch + 1} results: {train_metrics}")
+        summary_writer.add_scalar('accuracy/train', train_metrics['accuracy'], epoch)
+        summary_writer.add_scalar('f1_weighted/train', train_metrics['f1_weighted'], epoch)
+        summary_writer.add_scalar('f1_macro/train', train_metrics['f1_macro'], epoch)
+
+
+    # Evaluation and testing -------------------------
+    logger.info("***** Running evaluation set *****")
+    eval_metrics = compute_metrics(accelerator, model, eval_dataloader)
+    logger.info(f"Evalation results: {eval_metrics}")
+
+
+    logger.info("***** Running test set *****")
+    test_metrics = compute_metrics(accelerator, model, test_dataloader)
+    logger.info(f"Test results: {test_metrics}")
+    summary_writer.add_scalar('accuracy/eval', test_metrics['accuracy'], epoch)
+    summary_writer.add_scalar('f1_weighted/eval', test_metrics['f1_weighted'], epoch)
+    summary_writer.add_scalar('f1_macro/eval', test_metrics['f1_macro'], epoch)
+
+
+    summary_writer.close()
+
+    #save_model(output_directory_save_model)
+
+
+if __name__ == "__main__":
+    main()
