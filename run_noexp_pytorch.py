a833e1bb5decd80ba9fa31a4056eb55d09389d57
Dimitrios Pilitsis <dimipili@gmail.com>
2022-03-22 17:27:56 +0000

    Add per class metrics, clean up compute_metrics() and summary writers

--- run_noexp_pytorch.py
+++ run_noexp_pytorch.py
@@ -57,7 +57,7 @@ def create_smaller_dataset(tokenized_datasets):
     small_train_dataset = \
         tokenized_datasets["train"].shuffle(seed=42).select(range(80))
     small_test_dataset = \
-        tokenized_datasets["test"].shuffle(seed=42).select(range(10))
+        tokenized_datasets["test"].shuffle(seed=42).select(range(20))
 
     small_datasets = DatasetDict({"train": small_train_dataset, "test": small_test_dataset})
     return small_datasets
@@ -69,12 +69,18 @@ def create_smaller_dataset(tokenized_datasets):
 
 def compute_metrics(accelerator, model, dataloader):
     metric1 = load_metric("accuracy")
+
     metric2 = load_metric("f1")
     metric3 = load_metric("precision")
     metric4 = load_metric("recall")
 
+    metric2_weighted = load_metric("f1")
+    metric3_weighted = load_metric("precision")
+    metric4_weighted  = load_metric("recall")
+
     model.eval()
 
+    
     for batch in dataloader:
         with torch.no_grad():
             outputs = model(**batch)
@@ -91,20 +97,133 @@ def compute_metrics(accelerator, model, dataloader):
         metric4.add_batch(predictions=accelerator.gather(predictions),
             references=accelerator.gather(batch['labels']))
 
+        metric2_weighted.add_batch(predictions=accelerator.gather(predictions),
+            references=accelerator.gather(batch['labels']))
+        metric3_weighted.add_batch(predictions=accelerator.gather(predictions),
+            references=accelerator.gather(batch['labels']))
+        metric4_weighted.add_batch(predictions=accelerator.gather(predictions),
+            references=accelerator.gather(batch['labels']))
+
+
+    #Per class metrics
+    labels = list(range(0,8))
+    f1 = metric2.compute(average=None, labels=labels)["f1"]
+    precision = metric3.compute(average=None, labels=labels)["precision"]
+    recall = metric4.compute(average=None, labels=labels)["recall"]
+
+
     #Weighted metrics
     accuracy = metric1.compute()["accuracy"]
-    f1_weighted = metric2.compute(average="weighted")["f1"]
-    #precision = metric3.compute(average=None) #get per class results
-    precision_weighted = metric3.compute(average="weighted")["precision"]
-    recall_weighted = metric4.compute(average="weighted")["recall"]
+    f1_weighted = metric2_weighted.compute(average="weighted", labels=labels)["f1"]
+    precision_weighted = metric3_weighted.compute(average="weighted", labels=labels)["precision"]
+    recall_weighted = metric4_weighted.compute(average="weighted", labels=labels)["recall"]
 
     results = {"accuracy": accuracy, 
                 "f1_weighted": f1_weighted,
                 "precision_weighted": precision_weighted,
-                "recall_weighted": recall_weighted
+                "recall_weighted": recall_weighted,
+                "f1": f1,
+                "precision": precision,
+                "recall": recall
             }
 
     return results
+
+
+
+
+# Summary writers
+
+def summary_writer_train(summary_writer, train_metrics, epoch):
+    summary_writer.add_scalars("Train averaged statistics",
+                                    {"accuracy/train": train_metrics["accuracy"],
+                                     "f1_weighted/train": train_metrics["f1_weighted"],
+                                     "precision_weighted/train": train_metrics["precision_weighted"],
+                                     "recall_weighted/train":
+                                     train_metrics["recall_weighted"]},
+                                     epoch+1)
+     
+
+    summary_writer.add_scalars("Train f1 per class",
+                              {"f1_class_0/train": train_metrics["f1"][0],
+                              "f1_class_1/train" : train_metrics["f1"][1],
+                              "f1_class_2/train" : train_metrics["f1"][2],
+                              "f1_class_3/train" : train_metrics["f1"][3],
+                              "f1_class_4/train" : train_metrics["f1"][4],
+                              "f1_class_5/train" : train_metrics["f1"][5],
+                              "f1_class_6/train" : train_metrics["f1"][6],
+                              "f1_class_7/train" : train_metrics["f1"][7]},
+                              epoch+1)
+
+
+    summary_writer.add_scalars("Train precision per class",
+                       {"precision_class_0/train" : train_metrics["precision"][0],
+                        "precision_class_1/train" : train_metrics["precision"][1],
+                        "precision_class_2/train" : train_metrics["precision"][2],
+                        "precision_class_3/train" : train_metrics["precision"][3],
+                        "precision_class_4/train" : train_metrics["precision"][4],
+                        "precision_class_5/train" : train_metrics["precision"][5],
+                        "precision_class_6/train" : train_metrics["precision"][6],
+                        "precision_class_7/train" : train_metrics["precision"][7]},
+                        epoch+1)
+    
+
+    summary_writer.add_scalars("Train recall per class",
+                                {"recall_class_0/train" : train_metrics["recall"][0],
+                                 "recall_class_1/train" : train_metrics["recall"][1],
+                                 "recall_class_2/train" : train_metrics["recall"][2],
+                                 "recall_class_3/train" : train_metrics["recall"][3],
+                                 "recall_class_4/train" : train_metrics["recall"][4],
+                                 "recall_class_5/train" : train_metrics["recall"][5],
+                                 "recall_class_6/train" : train_metrics["recall"][6],
+                                 "recall_class_7/train" : train_metrics["recall"][7]},
+                                 epoch+1)
+
+
+
+def summary_writer_test(summary_writer, test_metrics, epoch):
+    summary_writer.add_scalars("Test averaged statistics",
+                                 {"accuracy/test": test_metrics["accuracy"],
+                                  "f1_weighted/test": test_metrics["f1_weighted"],
+                                  "precision_weighted/test": test_metrics["precision_weighted"],
+                                  "recall_weighted/test": test_metrics["recall_weighted"]},
+                                  epoch+1)
+
+
+    summary_writer.add_scalars("Test f1 per class",
+                              {"f1_class_0/test": test_metrics["f1"][0],
+                              "f1_class_1/test" : test_metrics["f1"][1],
+                              "f1_class_2/test" : test_metrics["f1"][2],
+                              "f1_class_3/test" : test_metrics["f1"][3],
+                              "f1_class_4/test" : test_metrics["f1"][4],
+                              "f1_class_5/test" : test_metrics["f1"][5],
+                              "f1_class_6/test" : test_metrics["f1"][6],
+                              "f1_class_7/test" : test_metrics["f1"][7]},
+                              epoch+1)
+
+
+    summary_writer.add_scalars("Test precision per class",
+                       {"precision_class_0/test" : test_metrics["precision"][0],
+                        "precision_class_1/test" : test_metrics["precision"][1],
+                        "precision_class_2/test" : test_metrics["precision"][2],
+                        "precision_class_3/test" : test_metrics["precision"][3],
+                        "precision_class_4/test" : test_metrics["precision"][4],
+                        "precision_class_5/test" : test_metrics["precision"][5],
+                        "precision_class_6/test" : test_metrics["precision"][6],
+                        "precision_class_7/test" : test_metrics["precision"][7]},
+                        epoch+1)
+   
+
+    summary_writer.add_scalars("Test recall per class",
+                            {"recall_class_0/test" : test_metrics["recall"][0],
+                             "recall_class_1/test" : test_metrics["recall"][1],
+                             "recall_class_2/test" : test_metrics["recall"][2],
+                             "recall_class_3/test" : test_metrics["recall"][3],
+                             "recall_class_4/test" : test_metrics["recall"][4],
+                             "recall_class_5/test" : test_metrics["recall"][5],
+                             "recall_class_6/test" : test_metrics["recall"][6],
+                             "recall_class_7/test" : test_metrics["recall"][7]},
+                             epoch+1)
 
 
 
@@ -118,7 +237,6 @@ def save_model(output_directory_save_model):
 
 
 # Main -------------------------------------------------------------------
-
 
 
 def main():
@@ -209,7 +327,6 @@ def main():
     tokenized_datasets.set_format("torch")
 
 
-
     # Dataloader --------------------------------------------------
 
     train_dataloader = DataLoader(tokenized_datasets['train'], shuffle=True, 
@@ -260,21 +377,15 @@ def main():
 
 
         train_metrics = compute_metrics(accelerator, model, train_dataloader)
-        logger.info(f"Epoch {epoch + 1} results: {train_metrics}")
-        summary_writer.add_scalar('accuracy/train', train_metrics['accuracy'], epoch)
-        summary_writer.add_scalar('f1_weighted/train', train_metrics['f1_weighted'], epoch)
-        summary_writer.add_scalar('precision_weighted/train', train_metrics['recall_weighted'], epoch)
-        summary_writer.add_scalar('recall_weighted/train', train_metrics['recall_weighted'], epoch)
+        logger.info(f"Epoch {epoch + 1} train results: {train_metrics}")
+        summary_writer_train(summary_writer, train_metrics, epoch)
 
-
-    # Testing -------------------------
-    logger.info("***** Running test set *****")
-    test_metrics = compute_metrics(accelerator, model, test_dataloader)
-    logger.info(f"Test results: {test_metrics}")
-    summary_writer.add_scalar('accuracy/test', test_metrics['accuracy'], epoch)
-    summary_writer.add_scalar('f1_weighted/test', test_metrics['f1_weighted'], epoch)
-    summary_writer.add_scalar('precision_weighted/test', test_metrics['precision_weighted'], epoch)
-    summary_writer.add_scalar('recall_weighted/test', test_metrics['recall_weighted'], epoch)
+        # Testing ----------------------------------------------------------------------
+        logger.info(f"***** Running test set Epoch {epoch + 1}*****")
+        test_metrics = compute_metrics(accelerator, model, test_dataloader)
+        logger.info(f"Epoch {epoch + 1} Test results: {test_metrics}")
+        
+        summary_writer_test(summary_writer, test_metrics, epoch)
 
 
     summary_writer.close()
