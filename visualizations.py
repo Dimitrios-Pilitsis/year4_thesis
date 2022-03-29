f040e2f8fbbdd6cb820e552118abbbf6f1bdfdf0
Dimitrios Pilitsis <dimipili@gmail.com>
2022-03-29 16:46:07 +0100

    Clean up functions significantly

--- visualizations.py
+++ visualizations.py
@@ -65,104 +65,20 @@ def summary_writer_pr_curves(summary_writer, accelerator, model, dataloader, epo
 
         labels.append(label)
         predictions.append(prediction)
-       
-    #Difference with normal way is that we need to deal with the last array
-    #seperately as it has a different size to the rest of the arrays
-    if predictions[0].shape != predictions[-1].shape:
-        pred_remainder = predictions[-1]
-        predictions = predictions[:-1]
-        
-        predictions = np.array(predictions)
-        predictions = np.reshape(predictions, (-1,
-            predictions.shape[2])).transpose()
-        
-        pred_remainder = np.array(pred_remainder).transpose()
-        
-        #Combine the 2 arrays to get (9, number of points in all batches)
-        predictions = np.hstack((predictions, pred_remainder))
 
-
-
-        label_remainder = labels[-1]
-        labels = labels[:-1]
-        
-        labels = np.hstack(np.array(labels, dtype=int))
-
-        labels = labels.reshape(labels.shape[0], 1) 
-
-        labels = labels.transpose()
-
-        label_remainder = label_remainder.reshape(label_remainder.shape[0], 1) 
-        label_remainder = label_remainder.transpose()
-
-        labels =np.hstack((labels, label_remainder))
-
-        labels = np.repeat(labels, 9, axis=0)
-
-    else:
-        predictions = np.array(predictions)
-        predictions = np.reshape(predictions, (-1,
-            predictions.shape[2])).transpose()
-        
-        labels = np.hstack(np.array(labels, dtype=int))
-        #labels = np.hstack(np.array(labels), dtype=int)
-        #Transform labels from vector to matrix i.e. (80,) => (80,1)
-        labels = labels.reshape(labels.shape[0], 1) 
-        #Transpose labels to become (1,80)
-        labels = labels.transpose()
-  
-
-        #Replicate labels 9 times (once for each class)
-        #labels becomes (1,80) => (9,80)
-        labels = np.repeat(labels, 9, axis=0)
-
-    """
-    # Prediction ---------------------------------------------------------
-    #Binarize predictions
-    predictions = np.array(predictions)
-    #predictions = np.array(predictions, dtype=np.float32)
-    #^ should be a 3D array (number of batches, batch size, number of classes)
-    #So for training it is (10, 8, 9)
-    #Convert then to (10*8,9), then transpose to (9, 10*8) and we done
     
-    #ADD DIMENSION HERE IF IT IS 2D OR SKIP BELOW STEP
+    #Flatten them by stacking them vertically, this converts it from 3D to 2D
+    #by converting it from (number of batches, batch size, number of classes)
+    #to (number of datapoints, number of classes)
+    #We then transpose the matrix to get classes on the rows
+    predictions = np.vstack(predictions).transpose()
     
-    print(predictions)
-    print(predictions.shape)
-    
-
-    #DO BELOW FOR CASES WHEN WE HAVE EQUAL NUMBER OF PREDICTION ARRAYS E.G.
-    #8,8,8
-
-    #Reshape from 3D to 2D with 9 columns (number of classes)
-    #i.e. remove number of batches so that it becomes each individual datapoint
-    predictions = np.reshape(predictions, (-1,
-        predictions.shape[2])).transpose()
-
-    print(predictions)
-    print(predictions.shape)
-    #predictions = np.reshape(predictions, (predictions.shape[0] *
-    #    predictions.shape[1], predictions.shape[2])).transpose()
-    #we also transposed ^ so that we can have it in the form 
-    #(number of classes, number of datapoints) so (9,80)
-    
-
-
-    # Label -----------------------------------------------------------
-    #Flattens array even when subarrays aren't of equal dimension (due to batch
-    # size), becomes numpy array of shape (N,1)
-    labels = np.hstack(np.array(labels, dtype=int))
-    #labels = np.hstack(np.array(labels), dtype=int)
-    #Transform labels from vector to matrix i.e. (80,) => (80,1)
-    labels = labels.reshape(labels.shape[0], 1) 
-    #Transpose labels to become (1,80)
-    labels = labels.transpose()
-  
-
-    #Replicate labels 9 times (once for each class)
-    #labels becomes (1,80) => (9,80)
+    #We use hstack to flatten the array, reshape to convert from vector to
+    #matrix, then repeat the matrix 9 times, once per class
+    labels = np.reshape(np.hstack(labels).astype(int), (1,-1))
     labels = np.repeat(labels, 9, axis=0)
-    """ 
+    #predictions_a = np.hstack(predictions)
+
     l = list(range(0,9))
     # Binarize labels to have 9 arrays, 1 for each label
     # where for class i, the values where i is we have as 1, all other classes
