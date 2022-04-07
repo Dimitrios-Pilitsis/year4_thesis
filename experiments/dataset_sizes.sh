3199c5cb0af5e6528a16f8c30c788609bcd0e3ef
Dimitrios Pilitsis <dimipili@gmail.com>
2022-04-07 15:20:26 +0300

    Fix dataset sizing issues and CSV saving issues

--- experiments/dataset_sizes.sh
+++ experiments/dataset_sizes.sh
@@ -1,14 +1,14 @@
 cd ..
-python3 create_dataset.py --dataset-percent 0.1 --output-noexp-directory ./dataset/crisis_dataset_size_0.1/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_0.1/exp/ 
-python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_0.1/noexp/ 
-python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_0.1/exp/ 
+python3 create_dataset.py --dataset-percent 0.1 --output-noexp-directory ./dataset/crisis_dataset_size_10/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_10/exp/ --noexp-csv-filepath ./dataset/dataset_noexp_size_10.csv --exp-csv-filepath ./dataset/dataset_exp_10.csv
+python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_10/noexp/ 
+python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_10/exp/ 
 
 
-python3 create_dataset.py --dataset-percent 0.5 --output-noexp-directory ./dataset/crisis_dataset_size_0.5/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_0.5/exp/ 
-python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_0.5/noexp/
-python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_0.5/exp/ 
+python3 create_dataset.py --dataset-percent 0.5 --output-noexp-directory ./dataset/crisis_dataset_size_50/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_50/exp/ --noexp-csv-filepath ./dataset/dataset_noexp_size_50.csv --exp-csv-filepath ./dataset/dataset_exp_50.csv
+python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_50/noexp/
+python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_50/exp/ 
 
 
-python3 create_dataset.py --dataset-percent 0.75 --output-noexp-directory ./dataset/crisis_dataset_size_0.75/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_0.75/exp/ 
-python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_0.75/noexp/
-python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_0.75/exp/ 
+python3 create_dataset.py --dataset-percent 0.75 --output-noexp-directory ./dataset/crisis_dataset_size_75/noexp/ --output-exp-directory ./dataset/crisis_dataset_size_75/exp/ --noexp-csv-filepath ./dataset/dataset_noexp_size_75.csv --exp-csv-filepath ./dataset/dataset_exp_75.csv
+python3 run.py --noexp-dataset-filepath ./dataset/crisis_dataset_size_75/noexp/
+python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_size_75/exp/ 
--- experiments/explanations_quality.sh
+++ experiments/explanations_quality.sh
@@ -1,4 +1,4 @@
 cd ..
-python3 create_dataset.py --explanations-filepath explanations/explanations_bad.txt --output-exp-directory ./dataset/crisis_dataset_bad/exp/
+python3 create_dataset.py --explanations-filepath explanations/explanations_bad.txt --exp-csv-filepath ./dataset/dataset_exp_bad.csv --output-exp-directory ./dataset/crisis_dataset_bad/exp/
 python3 run.py --exp-flag --exp-dataset-filepath ./dataset/crisis_dataset_bad/exp/ 
 
