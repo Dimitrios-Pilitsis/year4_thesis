fd8ef1ebd95c4379070c19162ba7916fac6140d1
Dimitrios Pilitsis <dimipili@gmail.com>
2022-05-05 22:44:43 +0300

    Obtain class counts of each category

--- error_analysis.py
+++ error_analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import argparse
 import random
+from collections import Counter
+
 
 from datasets import load_from_disk, load_metric, DatasetDict
 
@@ -41,6 +43,18 @@ def print_lengths(cc, ic, ci, ii):
     #Total lengths should be 17117
     print(len(cc)+len(ic)+len(ci)+len(ii))
 
+
+def print_counts(cc_labels, ic_labels, ci_labels, ii_labels):
+    cc_counter = Counter(cc_labels)
+    ic_counter = Counter(ic_labels)
+    ci_counter = Counter(ci_labels)
+    ii_counter = Counter(ii_labels)
+
+    print(cc_counter)
+    print(ic_counter)
+    print(ci_counter)
+    print(ii_counter)
+
 def print_tweet(dpi, ds):
     #go to the original dataset and find a datapoint of interest
     print(f'TWEET of datapoint {dpi}')
@@ -79,6 +93,11 @@ def main():
     ic = [] #points were incorrect and became correct
     ci = [] #points were correct and became incorrect
     ii = []
+    
+    cc_labels = []
+    ic_labels = []
+    ci_labels = []
+    ii_labels = []
 
     #Find how many datapoints were correctly and 
     #incorrectly classified by NoExp
@@ -96,17 +115,23 @@ def main():
         if (pred_noexp == label_noexp) and \
             (pred_exp == label_exp): 
             cc.append((idx_noexp_shuffled,idx_exp_shuffled))
+            cc_labels.append(label_noexp)
         elif (pred_noexp != label_noexp) and \
             (pred_exp == label_exp):
             ic.append((idx_noexp_shuffled,idx_exp_shuffled))
+            ic_labels.append(label_noexp)
         elif (pred_noexp == label_noexp) and \
             (pred_exp != label_exp):
             ci.append((idx_noexp_shuffled,idx_exp_shuffled))
+            ci_labels.append(label_noexp)
         else:
             ii.append((idx_noexp_shuffled,idx_exp_shuffled))
+            ii_labels.append(label_noexp)
+
+    #Print counts of each class for each category
+    print_counts(cc_labels, ic_labels, ci_labels, ii_labels)
     
     #Counts of each category
-
     #print_lengths(cc, ic, ci, ii)
     
     
